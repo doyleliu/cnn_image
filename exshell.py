ced6d75d28cea0a7e1572b61b91e3a8755c2301f
doyleliu <doyleliu96@gmail.com>
2017-07-17 18:05:00 +0800

    thread

--- exshell.py
+++ exshell.py
@@ -1,17 +1,30 @@
 import os 
 import logging 
 import subprocess 
+from threading import Thread 
+from Queue import Queue 
 
-log = logging.getLogger("Core.Analysis.Processing") 
-  
-INTERPRETER = "/DB/rhome/dliu/project/nowamagic_venv/bin/python"
-  
-  
-if not os.path.exists(INTERPRETER): 
-  log.error("Cannot find INTERPRETER at path \"%s\"." % INTERPRETER) 
-    
-processor = "features_tmp.py"
+num_threads=3 
+q=Queue() 
 
-for i in range(0,2):  
-	pargs = [INTERPRETER, processor,"-database","thumbnails","-index", "features_final.h5","-number",str(i)] 
-	subprocess.Popen(pargs)
+def workprocess(i,queue):
+  while True: 
+    nums = queue.get()
+    log = logging.getLogger("Core.Analysis.Processing") 
+    INTERPRETER = "/DB/rhome/dliu/project/nowamagic_venv/bin/python"
+    if not os.path.exists(INTERPRETER): 
+      log.error("Cannot find INTERPRETER at path \"%s\"." % INTERPRETER) 
+    processor = "features_tmp.py"
+	  pargs = [INTERPRETER, processor,"-database","thumbnails","-index", "features_final.h5","-number",str(nums)] 
+	  subprocess.Popen(pargs)
+    queue.task_done()
+
+
+for i in range(num_threads): 
+  t=Thread(target=workprocess, args=(i,q)) 
+  t.setDaemon(True) 
+  t.start() 
+for i in range(0,199):
+  q.put(i)
+
+q.join();print 'Done'
