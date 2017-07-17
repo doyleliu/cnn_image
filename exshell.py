347a04a465a15be677a72a3f571d48fff9842c79
doyleliu <doyleliu96@gmail.com>
2017-07-17 18:22:47 +0800

    process

--- exshell.py
+++ exshell.py
@@ -4,27 +4,19 @@ import subprocess
 from threading import Thread 
 from Queue import Queue 
 
-num_threads=3 
-q=Queue() 
-
-def workprocess(i,queue):
-  while True: 
-    nums = queue.get()
-    log = logging.getLogger("Core.Analysis.Processing") 
-    INTERPRETER = "/DB/rhome/dliu/project/nowamagic_venv/bin/python"
-    if not os.path.exists(INTERPRETER): 
-      log.error("Cannot find INTERPRETER at path \"%s\"." % INTERPRETER) 
-    processor = "features_tmp.py"
-    pargs = [INTERPRETER, processor,"-database","thumbnails","-index", "features_final.h5","-number",str(nums)] 
-    subprocess.Popen(pargs)
-    queue.task_done()
 
 
-for i in range(num_threads): 
-  t=Thread(target=workprocess, args=(i,q)) 
-  t.setDaemon(True) 
-  t.start() 
-for i in range(0,2):
-  q.put(i)
+def workprocess(nums):
+  log = logging.getLogger("Core.Analysis.Processing") 
+  INTERPRETER = "/DB/rhome/dliu/project/nowamagic_venv/bin/python"
+  if not os.path.exists(INTERPRETER): 
+    log.error("Cannot find INTERPRETER at path \"%s\"." % INTERPRETER) 
+  processor = "features_tmp.py"
+  pargs = [INTERPRETER, processor,"-database","thumbnails","-index", "features_final.h5","-number",str(nums)] 
+  subprocess.Popen(pargs)
+  
+for i in range(0,1)
+  workprocess(i)
+  print "The %d process"(i)
 
-q.join();print 'Done'
+print "Done!"
